f4821b1f0750617dd176b4089613f36b16ac8ce1
SHRI171981 <shrinidhikumar.2003@gmail.com>
2026-02-17 23:46:09 +0530

    video2: URL routing

--- crm/accounts/views.py
+++ crm/accounts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    return HttpResponse("Welcome to the CRM Home Page!")
+
+
+def products(request):
+    return HttpResponse("This is the products page.")
+
+
+def customers(request):
+    return HttpResponse("This is the customers page.")
